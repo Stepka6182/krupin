58dfb5ad7812912eca7be44aa0bf2f5233b539b9
Stepka6182 <kruola52@gmail.com>
2026-04-05 19:25:21 +0500

    LB_8 C#

---
+++ ConsoleApp8/ConsoleApp8/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.IO;
+using System.IO.Pipes;
+
+namespace Server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var pipe = new NamedPipeServerStream("MyPipe"))
+            {
+                Console.WriteLine("Сервер ждет клиента...");
+                pipe.WaitForConnection();  // Ждем подключения (теория, стр.3)
+                Console.WriteLine("Клиент подключен!");
+
+                using (var writer = new StreamWriter(pipe))
+                using (var reader = new StreamReader(pipe))
+                {
+                    string msg = "";
+                    int fl = 0;  // Флаг очередности (теория, стр.8)
+
+                    while (msg != "ПОКА")
+                    {
+                        if (fl == 0)
+                        {
+                            Console.Write("Сервер: ");
+                            msg = Console.ReadLine();
+                            writer.WriteLine(msg);
+                            writer.Flush();  // Очистка буфера (теория, стр.7)
+                            fl = 1;
+                        }
+                        else
+                        {
+                            msg = reader.ReadLine();
+                            Console.WriteLine("Клиент: " + msg);
+                            fl = 0;
+                        }
+                    }
+                }
+            }
+            Console.WriteLine("Сеанс закончен");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ConsoleApp8/client/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.IO;
+using System.IO.Pipes;
+
+namespace Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var pipe = new NamedPipeClientStream("MyPipe"))
+            {
+                Console.WriteLine("Клиент подключается...");
+                pipe.Connect();  // Подключаемся к серверу (теория, стр.4)
+                Console.WriteLine("Подключено!");
+
+                using (var reader = new StreamReader(pipe))
+                using (var writer = new StreamWriter(pipe))
+                {
+                    string msg = "";
+                    int fl = 0;  // Флаг очередности (теория, стр.8)
+
+                    while (msg != "ПОКА")
+                    {
+                        if (fl == 0)
+                        {
+                            msg = reader.ReadLine();
+                            Console.WriteLine("Сервер: " + msg);
+                            fl = 1;
+                        }
+                        else
+                        {
+                            Console.Write("Клиент: ");
+                            msg = Console.ReadLine();
+                            writer.WriteLine(msg);
+                            writer.Flush();
+                            fl = 0;
+                        }
+                    }
+                }
+            }
+            Console.WriteLine("Сеанс закончен");
+            Console.ReadLine();
+        }
+    }
+}
